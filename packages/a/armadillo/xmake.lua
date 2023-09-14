8b6139c6e76bae168e598af5095944204d6cfaa6
welann <49773825+welann@users.noreply.github.com>
2023-09-15 00:11:13 +0800

    Update armadillo/xmake.lua

--- packages/a/armadillo/xmake.lua
+++ packages/a/armadillo/xmake.lua
@@ -4,8 +4,10 @@ package("armadillo")
     set_homepage("http://arma.sourceforge.net/")
     set_description("C++ library for linear algebra & scientific computing")
     set_license("Apache-2.0")
+              
+    add_urls("https://sourceforge.net/project/arma/armadillo-$(version).tar.xz")
 
-    add_urls("http://sourceforge.net/projects/arma/files/armadillo-$(version).tar.xz")
+    add_versions("12.6.4", "eb7f243ffc32f18324bc7fa978d0358637e7357ca7836bec55b4eb56e9749380")
     add_versions("11.2.3", "4c2e97ce60707fc1f348f44f7af0cb6d2466d0aad0d0ea4bf5d5dc180e6cba41")
     add_versions("10.8.1", "5087ab5a2268e5ce71798c1afcb6d1fb246463f8dc88a60db49a083600f98332")
     add_versions("10.7.0", "9bf60db6fd237721908747a0e56797b97b7ceae3603f2cca0b012a3b88265d3f")
